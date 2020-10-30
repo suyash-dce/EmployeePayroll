1fd72222435ee609ea21c5367ce2d279a57f4ce4
suyash-dce <jain.suyash28@gmail.com>
2020-10-30 19:59:48 +0530

    Added: DB Service connection Host File added to Main

---
+++ main/java/com/JDBCEmpPayroll/EmployeePayrollDBService.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.*;
+
+public class EmployeePayrollDBService {
+	public List<EmployeePayrollData> readData() {
+		String sql = "SELECT* FROM employeepayroll;";
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+		try(Connection connection = this.getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sql);
+			while(resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				double salary = resultSet.getDouble("salary");
+				LocalDate startDate = resultSet.getDate("start").toLocalDate();
+				employeePayrollList.add(new EmployeePayrollData(id, name, salary,startDate));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return employeePayrollList;
+	}
+
+	private Connection getConnection() throws SQLException {
+		String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll?useSSL=false";
+		String userName = "root";
+		String password = "jain1234";
+		Connection connection;
+		System.out.println("Connecting to database: " + jdbcURL);
+		connection = DriverManager.getConnection(jdbcURL, userName, password);
+		System.out.println("Connection successful: " + connection);
+		return connection;
+	}
+}
