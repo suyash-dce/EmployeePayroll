cf55d68dcdf30a69f711ea5816d39efd118014fe
suyash-dce <jain.suyash28@gmail.com>
2020-11-01 18:34:29 +0530

    Updated: Test File to Check Functionality of Aggregate Functions in Project is Added

--- test/java/com/JDBCEmpPayroll/EmployeePayrollServiceTest.java
+++ test/java/com/JDBCEmpPayroll/EmployeePayrollServiceTest.java
@@ -6,15 +6,18 @@ import java.util.*;
 public class EmployeePayrollServiceTest {
 	EmployeePayRollService empPayrollService;
 	List<EmployeePayRollData> empPayrollList;
+	Map<String, Double> empPayrollDataByGenderMap;
 
 	@Before
 	public void Initializer() {
 		empPayrollService = new EmployeePayRollService();
 		empPayrollList = empPayrollService.readEmployeePayrollData(IOService.DB_IO);
+
 	}
 
 	@Test
 	public void givenEmpPayrollDB_WhenRetrieved_ShouldMatchEmpCount() {
+
 		assertEquals(5, empPayrollList.size());
 		System.out.println(empPayrollList);
 	}
@@ -23,7 +26,7 @@ public class EmployeePayrollServiceTest {
 	public void givenNewSalary_WhenUpdated_ShouldSyncWithDB() throws SQLException {
 		EmployeePayRollService empPayRollService = new EmployeePayRollService();
 		List<EmployeePayRollData> empPayrollList = empPayRollService.readEmployeePayrollData(IOService.DB_IO);
-		empPayRollService.updateEmployeeSalary("Suyash", 3000001.0);
+		empPayRollService.updateEmployeeSalary("Suyash", 2500000);
 		boolean isSynced = empPayRollService.isEmpPayrollSyncedWithDB("Suyash");
 		assertTrue(isSynced);
 	}
@@ -36,5 +39,33 @@ public class EmployeePayrollServiceTest {
 		LocalDate enDate = LocalDate.now();
 		empPayrollList = empPayRollService.getEmpPayrollDataForDataRange(startDate, enDate);
 		assertEquals(4, empPayrollList.size());
+	}
+
+	@Test
+	public void givenEmployeeDB_WhenRetrievedSum_ShouldReturnSumByGender() throws EmployeePayrollException {
+		empPayrollDataByGenderMap = empPayrollService.getSumOfDataGroupedByGender(IOService.DB_IO, "basicPay");
+		assertEquals(2500000, empPayrollDataByGenderMap.get("M"), 0.0);
+		assertEquals(6000000, empPayrollDataByGenderMap.get("F"), 0.0);
+	}
+
+	@Test
+	public void givenEmployeeDB_WhenRetrievedAvg_ShouldReturnAvgByGender() throws EmployeePayrollException {
+		empPayrollDataByGenderMap = empPayrollService.getAvgOfDataGroupedByGender(IOService.DB_IO, "basicPay");
+		assertEquals(2500000, empPayrollDataByGenderMap.get("M"), 0.0);
+		assertEquals(3000000, empPayrollDataByGenderMap.get("F"), 0.0);
+	}
+
+	@Test
+	public void givenEmployeeDB_WhenRetrievedMaxMin_ShouldReturnMaxByGender() throws EmployeePayrollException {
+		empPayrollDataByGenderMap = empPayrollService.getMAXOfDataGroupedByGender(IOService.DB_IO, "basicPay");
+		assertEquals(2500000, empPayrollDataByGenderMap.get("M"), 0.0);
+		assertEquals(3000000, empPayrollDataByGenderMap.get("F"), 0.0);
+	}
+
+	@Test
+	public void givenEmployeeDB_WhenRetrievedCount_ShouldReturnCountByGender() throws EmployeePayrollException {
+		empPayrollDataByGenderMap = empPayrollService.getCountOfDataGroupedByGender(IOService.DB_IO, "id");
+		assertEquals(2, empPayrollDataByGenderMap.get("M"), 0.0);
+		assertEquals(3, empPayrollDataByGenderMap.get("F"), 0.0);
 	}
 }
